2ed0bfecafa8e34f80d8b17f282740578a0ca27d
zizi <mongni08@gmail.com>
2025-07-21 16:09:44 +0900

    추가

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
 import streamlit as st
 
+import os
+
 st.title('배포 테스트')
 st.write('배포 테스트용 앱입니다.')
+
+st.write('추가 커밋')
+
+api_key = os.environ.get('API_SECRET_KEY')
+
+st.write(f'api key = {api_key}')
+
+if not os.environ.get('DB_HOST'):
+    st.write('디비 접속 정보가 없습니다.')
+else:
+    conn = pymysql.connect(host=os.environ.get('DB_HOST'))
